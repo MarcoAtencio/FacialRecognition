7aa16f2513700efba23644f1e296bb13d581fc50
MarcoAtencio <n00049623@upn.pe>
2020-04-22 04:18:08 -0500

    v1.0

--- face.py
+++ face.py
@@ -3,7 +3,7 @@ from playsound import playsound
 
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
-video = cv2.VideoCapture(0)
+video = cv2.VideoCapture(0) #0 = camara default
 estadoanterior = ""
 estadoactual = ""
 mostrar=""
@@ -14,40 +14,43 @@ while video.isOpened():
         hh, ww = gray.shape
         faces = face_cascade.detectMultiScale(gray, 1.3, 5)
         for (x, y, w, h) in faces:
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
-            cv2.line(frame, (int(ww / 3), 0), (int(ww / 3), hh), (0, 0, 255))
-            cv2.line(frame, (int(ww*2 / 3), 0), (int(ww*2 / 3), hh), (0, 0, 255))
-            if x <= ww / 3:
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2) # rectangle face
+            #divide the screem into 3
+            cv2.line(frame, (int(ww / 3), 0), (int(ww / 3), hh), (0, 0, 255)) # first line
+            cv2.line(frame, (int(ww*2 / 3), 0), (int(ww*2 / 3), hh), (0, 0, 255)) # second line
+            if x <= ww / 3: #first space
                 estadoactual = "I"
                 if not estadoactual == estadoanterior:
                     mostrar = "I: (" + str(x) + "," + str(y) + ")"
                     playsound("izquierdox.mp3")
                     estadoanterior = estadoactual
 
-            elif x+w > ww * 2 /3:
+            elif x+w > ww * 2 /3: #third space
                 estadoactual = "D"
                 if not estadoactual == estadoanterior:
                     mostrar = "D: (" + str(x) + "," + str(y) + ")"
                     playsound("derechox.mp3")
                     estadoanterior = estadoactual
 
-            else :
+            else : #second space
                 estadoactual = "C"
                 if not estadoactual == estadoanterior:
                     mostrar = "C: (" + str(x) + "," + str(y) + ")"
                     playsound("espera.mp3")
                     estadoanterior = estadoactual
 
+            #render rectangle face
             cv2.putText(frame, mostrar, (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (0, 0, 255), 1, cv2.LINE_AA)
             cv2.rectangle(gray, (x, y), (x + w, y + h), (255, 0, 0), 2)
 
             roi_gray = gray[y:y + h, x:x + w]
             roi_color = frame[y:y + h, x:x + w]
+            #render rectangle eyes
             eyes = eye_cascade.detectMultiScale(roi_gray)
             for (ex, ey, ew, eh) in eyes:
                 cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
         cv2.imshow('Video', frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if cv2.waitKey(1) & 0xFF == ord('q'): #press q to finish
         break
 video.release()
 cv2.destroyAllWindows()
